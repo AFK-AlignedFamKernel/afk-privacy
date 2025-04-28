89f511d2c89546cdba2cc93241026b8f50e6d9ac
MSGhais <msghais135@gmail.com>
2025-04-28 13:57:17 +0200

    ui home

---
+++ app/pages/organizations/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Head from "next/head";
+import MessageList from "../../components/message-list";
+
+export default function OrganizationsPage() {
+  return (
+    <>
+      <Head>
+        <title>AFK - Anonymous messages from your coworkers, country and people</title>
+        <meta name="description" content="AFK is a platform for anonymous messages from your coworkers, country and people" />
+      </Head>
+
+      <div className="home-page">
+        <MessageList showMessageForm />
+      </div>
+    </>
+  );
+}
