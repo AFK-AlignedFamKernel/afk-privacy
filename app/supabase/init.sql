38c6519652d3c1268491f274a9c6f73aec5ba5c2
MSGhais <msghais135@gmail.com>
2025-04-30 20:07:45 +0200

    wip

--- app/supabase/init.sql
+++ app/supabase/init.sql
@@ -29,6 +29,7 @@ CREATE TABLE IF NOT EXISTS memberships (
 CREATE TABLE IF NOT EXISTS organizations (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name TEXT NOT NULL,
+    description TEXT,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     total_polls INTEGER NOT NULL DEFAULT 0,
     total_votes INTEGER NOT NULL DEFAULT 0,
