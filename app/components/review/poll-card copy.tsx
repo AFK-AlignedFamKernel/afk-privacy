143a925d3572568f35be7a5b6de93066f9def399
MSGhais <msghais135@gmail.com>
2025-05-02 14:22:44 +0200

    fix ui card

--- app/components/review/poll-card copy.tsx
+++ app/components/review/poll-card copy.tsx
@@ -5,7 +5,6 @@ import { signMessageSelfXyz } from "../../lib/zk-did";
 import { countryNames, domainNames } from "../../lib/constants";
 import Link from 'next/link';
 import COUNTRY_DATA from '@/assets/country';
-import CryptoLoading from '../small/crypto-loading';
 type ReviewMetadata = {
   rating?: number;
 };
@@ -44,10 +43,9 @@ type ReviewCardProps = {
   review: Review;
   isInternal?: boolean;
   onVote?: (pollId: string, option: string) => Promise<void>;
-  isShowLink?: boolean;
 };
 
-const PollCard: React.FC<ReviewCardProps> = ({ review, isInternal, onVote, isShowLink = true }) => {
+const PollCard: React.FC<ReviewCardProps> = ({ review, isInternal, onVote }) => {
   const [selectedOptions, setSelectedOptions] = useState<Set<string>>(new Set());
   const [error, setError] = useState<string | null>(null);
   const [isVoting, setIsVoting] = useState(false);
@@ -59,22 +57,6 @@ const PollCard: React.FC<ReviewCardProps> = ({ review, isInternal, onVote, isSho
 
   const [isLoadingStats, setIsLoadingStats] = useState(false);
   const [isExpandedRequirements, setIsExpandedRequirements] = useState(false)
-  const [countryFlags, setCountryFlags] = useState<string[]>([]);
-
-  const [isLoadingResults, setIsLoadingResults] = useState(false);
-
-  useEffect(() => {
-
-
-    if (review.is_show_results_publicly) {
-      console.log("review.is_show_results_publicly", review.is_show_results_publicly);
-      setIsLoadingResults(true);
-      handleResultStats();
-      setIsShowStats(true);
-      setIsLoadingResults(false);
-    }
-  }, [review.is_show_results_publicly]);
-
 
   const handleOnVote = async (pollId: string, option: string, options?: string) => {
     console.log("handleOnVote")
@@ -272,12 +254,21 @@ const PollCard: React.FC<ReviewCardProps> = ({ review, isInternal, onVote, isSho
     }
   }
 
+  useEffect(() => {
+
+
+    if (review.is_show_results_publicly) {
+      console.log("review.is_show_results_publicly", review.is_show_results_publicly);
+      handleResultStats();
+      setIsShowStats(true);
+    }
+  }, [review.is_show_results_publicly]);
+
 
   const handleResultStats = async () => {
 
     try {
       // console.log("handleResultStats", review);
-      setIsLoadingResults(true);
       const signedMessage = await handleSignMessage();
       // console.log("signedMessage", signedMessage);
 
@@ -303,26 +294,19 @@ const PollCard: React.FC<ReviewCardProps> = ({ review, isInternal, onVote, isSho
       setStatsData(data);
     } catch (error) {
       console.error("Error fetching result stats", error);
-    } finally {
-      setIsLoadingResults(false);
     }
 
   }
 
   const renderPollRequirements = () => {
-
-    if (!review) return null;
     // console.log("review", review);
     const requirements = [];
-    // console.log("review.selected_countries", review.selected_countries);
-    const countryNamesList = review.selected_countries
-    const countryFlags = review.selected_countries?.map(code => COUNTRY_DATA[code?.toUpperCase()]?.flag ?? "");
-    console.log("countryFlags", countryFlags);
-    const isCountrySelection = review?.selected_countries && review?.selected_countries?.length > 0;
-    const isOrganizationSelection = review?.selected_organizations && review?.selected_organizations?.length > 0;
-    //  setCountryFlags(countryFlags);
-    if (review?.selected_countries?.length) {
 
+    if (review?.selected_countries?.length) {
+      // console.log("review.selected_countries", review.selected_countries);
+      const countryNamesList = review.selected_countries
+
+      const countryFlags = review.selected_countries.map(code => COUNTRY_DATA[code?.toUpperCase()]?.flag ?? "");
       // console.log("countryFlags", countryFlags);
       requirements.push(`Countries: ${review.selected_countries?.length ? review.selected_countries.map(code => `${COUNTRY_DATA[code?.toUpperCase()]?.name || code} ${COUNTRY_DATA[code?.toUpperCase()]?.flag ?? ""}`).join(", ") : 'All'}`);
     }
@@ -381,13 +365,10 @@ const PollCard: React.FC<ReviewCardProps> = ({ review, isInternal, onVote, isSho
       <div className="poll-requirements"
         onClick={() => setIsExpandedRequirements(!isExpandedRequirements)}
       >
-        <div style={{ display: "flex", alignItems: "center", gap: "10px" }}
-        >
-          <p>
-            <IonIcon name={isExpandedRequirements ? "chevron-down-outline" : "chevron-forward-outline"} />
-            Requirements</p>
+        <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+          <IonIcon name={isExpandedRequirements ? "chevron-down-outline" : "chevron-forward-outline"} />
+          <p>Requirements</p>
         </div>
-        {/* <p>{isCountrySelection && "Countries"} {isOrganizationSelection && "Organizations"}</p> */}
 
         {isExpandedRequirements &&
           <ul>
@@ -455,6 +436,35 @@ const PollCard: React.FC<ReviewCardProps> = ({ review, isInternal, onVote, isSho
     );
   };
 
+  const renderPollOverview = () => {
+
+    const totalVotes = review.total_votes || 0;
+    const optionVotes = review.option_votes || {};
+
+
+    return (
+      <div className="poll-voting-stats">
+
+        <div className="poll-stats">
+          {/* <div className="stat-item total-votes">
+            <IonIcon name="bar-chart-outline" />
+            <span>{review.total_votes || 0} votes</span>
+          </div> */}
+          <div className="stat-item">
+            <IonIcon name={review.multiselect ? "checkbox-outline" : "radio-button-on-outline"} />
+            <span>{review.multiselect ? "Multiple choice" : "Single choice"}</span>
+          </div>
+          {/* <div className="stat-item">
+            <IonIcon name="time-outline" />
+            <span>Ends: {new Date(review.ends_at!).toLocaleString()}</span>
+          </div> */}
+        </div>
+      </div>
+    )
+
+  }
+
+
 
   const renderPollDescription = () => {
     return (
@@ -463,13 +473,11 @@ const PollCard: React.FC<ReviewCardProps> = ({ review, isInternal, onVote, isSho
         {review.description && (
           <>
             <div
-              style={{ display: "flex", alignItems: "center", gap: "10px", fontSize: "12px" }}
-              // className={`expand-button ${isExpandedDescription ? 'expanded' : ''}`}
+              className={`expand-button ${isExpandedDescription ? 'expanded' : ''}`}
               onClick={() => {
                 setIsExpandedDescription(!isExpandedDescription)
               }}
             >
-
               <p className="poll-description">
                 {review.description.length > 30 && !isExpandedDescription
                   ? `${review.description.slice(0, 30)}...`
@@ -494,6 +502,7 @@ const PollCard: React.FC<ReviewCardProps> = ({ review, isInternal, onVote, isSho
   }
 
 
+
   return (
     <div className="poll-card">
       <div className="poll-header-container">
@@ -501,66 +510,84 @@ const PollCard: React.FC<ReviewCardProps> = ({ review, isInternal, onVote, isSho
           <div className="poll-title-section">
             <h3 className="poll-title">{review.title}</h3>
             {renderPollDescription()}
-            <div style={{ display: "flex", flexDirection: "column", gap: "10px", fontSize: "12px" }}>
 
-
-              <div className="poll-stats-container" style={{ gap: "10px", display: "flex", alignItems: "flex-start", borderRadius: "8px", backgroundColor: "var(--background-secondary)", paddingTop: "3px", paddingBottom: "3px", }}>
-
-                <div>
-                  <IonIcon name="person-outline" />
-                  <span>By: {getAnonymousName()}</span>
-
-                </div>
-                <div className="stat-item">
-                  <IonIcon name="time-outline" />
-                  <span>Created at: {new Date(review.created_at!).toLocaleDateString()}</span>
-                </div>
-                <div className="stat-item">
-                  <IonIcon name="time-outline" />
-                  <span>Ends: {new Date(review.ends_at!).toLocaleString()}</span>
-                </div>
-
+       
+            {/* <div className="poll-stats-container" style={{ gap: "10px", display: "flex", alignItems: "flex-start" }}>
+              <div className="poll-date">
+                <IonIcon name="eye-outline" />
+                <span>{review?.is_show_results_publicly ? "Public" : "Private"}</span>
               </div>
 
-              <div className="poll-stats-container" style={{ gap: "10px", display: "flex", alignItems: "flex-start", borderRadius: "8px", backgroundColor: "var(--background-secondary)", paddingTop: "10px", paddingBottom: "10px", }}>
-                <div className="poll-date">
-                  <IonIcon name="eye-outline" />
-                  <span>{review?.is_show_results_publicly ? "Public" : "Private"}</span>
-                </div>
-
-                <div className="poll-select-type">
-                  <IonIcon name={review.multiselect ? "checkbox-outline" : "radio-button-on-outline"} />
-                  <span>{review.multiselect ? "Multiple choice" : "Single choice"}</span>
-                </div>
-
+              <div className="poll-select-type">
+                <IonIcon name={review.multiselect ? "checkbox-outline" : "radio-button-on-outline"} />
+                <span>{review.multiselect ? "Multiple choice" : "Single choice"}</span>
               </div>
 
+            </div> */}
+
+          </div>
+          
+          {/* <div className="poll-actions">
+
+            <div className="poll-meta poll-author">
+              <IonIcon name="person-outline" />
+              <span>{getAnonymousName()}</span>
+            </div> <button
+              className={`expand-button ${isExpanded ? 'expanded' : ''}`}
+              onClick={() => setIsExpanded(!isExpanded)}
+            >
+              <IonIcon name="chevron-down-outline" />
+              <span>{isExpanded ? 'Show Less' : 'Show More'}</span>
+            </button>
+          </div> */}
+
+        </div>
+
+        <div className="poll-stats-container">
+
+          <p>Details</p>
+          <div className="poll-stats-container" style={{ gap: "10px", display: "flex", alignItems: "flex-start", borderRadius: "8px", backgroundColor: "var(--background-secondary)", paddingTop: "10px", paddingBottom: "10px", }}>
+            <div className="poll-date">
+              <IonIcon name="eye-outline" />
+              <span>{review?.is_show_results_publicly ? "Public" : "Private"}</span>
+            </div>
+
+            <div className="poll-select-type">
+              <IonIcon name={review.multiselect ? "checkbox-outline" : "radio-button-on-outline"} />
+              <span>{review.multiselect ? "Multiple choice" : "Single choice"}</span>
             </div>
 
           </div>
         </div>
+
+
+        {renderPollRequirements()}
+
+
       </div>
-
-
-      {renderPollRequirements()}
 
       {
         isExpanded &&
         <>
           <div className="details-content">
-            {isLoadingResults && <CryptoLoading />}
-            {!isLoadingResults && renderPollStats()}
+            {/* {renderPollOverview()} */}
+            {renderPollStats()}
           </div>
         </>
       }
 
+      {renderPollDescription()}
+
+
       <div className="poll-form">
-        {/* {renderPollDescription()} */}
+
         {renderPollOptions()}
         {error && <div className="error-message">{error}</div>}
+
       </div>
 
       <div>
+
         <button
           className={`expand-button ${isExpanded ? 'expanded' : ''}`}
           onClick={() => {
@@ -573,18 +600,28 @@ const PollCard: React.FC<ReviewCardProps> = ({ review, isInternal, onVote, isSho
           <span>{isExpanded ? 'Hide results' : 'Show results'}</span>
         </button>
       </div>
+      {/* 
+      <button
+          className={`expand-button ${isExpanded ? 'expanded' : ''}`}
+          onClick={() => {
+            setIsExpanded(!isExpanded)
+            handleResultStats();
+            // setIsShowStats(false)
+          }}
+        >
+          <IonIcon name="chevron-down-outline" />
+          <span>{isExpanded ? 'Hide results' : 'Show results'}</span>
+        </button> */}
 
-      {isShowLink && (
 
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "2  0px" }}>
-          <p
-            className="submit-vote-button"
-          >
-            <IonIcon name="open-outline" />
-            <Link href={`/poll/${review.id}`}>View poll</Link>
-          </p>
-        </div>
-      )}
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "2  0px" }}>
+        <p
+          className="submit-vote-button"
+        >
+          <IonIcon name="open-outline" />
+          <Link href={`/poll/${review.id}`}>View poll</Link>
+        </p>
+      </div>
 
     </div >
   );
